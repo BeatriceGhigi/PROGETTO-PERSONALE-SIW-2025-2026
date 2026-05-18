95fade7714cba0c9e094ee56136ec0e804ff2d74
BeatriceCiucciarelli <bea.ciucciarelli@stud.uniroma3.it>
2026-05-18 15:44:24 +0200

    Update Categoria.java
    implementazione entità Categoria

--- src/main/java/it/uniroma3/ProdottiVegani/model/Categoria.java
+++ src/main/java/it/uniroma3/ProdottiVegani/model/Categoria.java
@@ -1,5 +1,81 @@
 package it.uniroma3.ProdottiVegani.model;
 
-public class Categoria {
+import java.util.List;
+import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Categoria {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(length = 2000)
+    private String descrizione;
+
+    @OneToMany(mappedBy = "category")
+    private List<Prodotto> prodotti;
+
+    // COSTRUTTORE
+    public Categoria() {
+        
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public List<Prodotto> getProdotti() {
+        return prodotti;
+    }
+
+    public void setProdotti(List<Prodotto> prodotti) {
+        this.prodotti = prodotti;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Categoria other = (Categoria) obj;
+        return Objects.equals(id, other.id);
+    }
 }
