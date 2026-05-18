949ee93c1dd59be10daa671645bb2a5f1eee0ff3
BeatriceCiucciarelli <bea.ciucciarelli@stud.uniroma3.it>
2026-05-18 16:57:24 +0200

    Update Routine.java
    Implementazione Routine

--- src/main/java/it/uniroma3/ProdottiVegani/model/Routine.java
+++ src/main/java/it/uniroma3/ProdottiVegani/model/Routine.java
@@ -1,5 +1,105 @@
 package it.uniroma3.ProdottiVegani.model;
 
-public class Routine {
+import java.util.Objects;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+@Entity
+public class Routine {
+	public enum TipoRoutine {
+        MATTINA,
+        SERA,
+        SETTIMANALE
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoRoutine tipo;
+
+    @Column(length = 2000)
+    private String descrizione;
+
+    @ManyToOne
+    @JoinColumn(name = "utente_id", nullable = false)
+    private Utente utente;
+
+
+    // COSTRUTTORE
+    public Routine() {
+        
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public TipoRoutine getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoRoutine tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Utente getUtente() {
+        return utente;
+    }
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Routine other = (Routine) obj;
+        return Objects.equals(id, other.id);
+    }
 }
