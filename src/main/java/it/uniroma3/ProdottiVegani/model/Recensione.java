6369b95057c0770748bdbe146fb24bfe7d126496
BeatriceCiucciarelli <bea.ciucciarelli@stud.uniroma3.it>
2026-05-18 16:24:43 +0200

    Update Recensione.java
    Implementazione Entità Recensione

--- src/main/java/it/uniroma3/ProdottiVegani/model/Recensione.java
+++ src/main/java/it/uniroma3/ProdottiVegani/model/Recensione.java
@@ -1,5 +1,117 @@
 package it.uniroma3.ProdottiVegani.model;
 
-public class Recensione {
+import java.time.LocalDateTime;
+import java.util.Objects;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Recensione {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer voto;
+
+    @Column(length = 2000)
+    private String commento;
+    
+    @Column(nullable = false)
+	private LocalDateTime dataCreazione;
+	 
+	 @Column(nullable = false)
+	 private LocalDateTime dataUltimaModifica;
+
+    @ManyToOne
+    @JoinColumn(name = "prodotto_id", nullable = false)
+    private Prodotto prodotto;
+
+    @ManyToOne
+    @JoinColumn(name = "utente_id", nullable = false)
+    private Utente autore;
+
+    // COSTRUTTORE
+    public Recensione() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVoto() {
+        return voto;
+    }
+
+    public void setVoto(Integer voto) {
+        this.voto = voto;
+    }
+
+    public String getCommento() {
+        return commento;
+    }
+
+    public void setCommento(String commento) {
+        this.commento = commento;
+    }
+
+    public Prodotto getProdotto() {
+        return prodotto;
+    }
+
+    public void setProdotto(Prodotto prodotto) {
+        this.prodotto = prodotto;
+    }
+
+    public Utente getUtente() {
+        return autore;
+    }
+
+    public void setUtente(Utente utente) {
+        this.autore = utente;
+    }
+
+    public LocalDateTime getDataCreazione() {
+		return dataCreazione;
+	}
+
+	public void setDataCreazione(LocalDateTime dataCreazione) {
+		this.dataCreazione = dataCreazione;
+	}
+
+	public LocalDateTime getDataUltimaModifica() {
+		return dataUltimaModifica;
+	}
+
+	public void setDataUltimaModifica(LocalDateTime dataUltimaModifica) {
+		this.dataUltimaModifica = dataUltimaModifica;
+	}
+
+	@Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Recensione other = (Recensione) obj;
+        return Objects.equals(id, other.id);
+    }
 }
