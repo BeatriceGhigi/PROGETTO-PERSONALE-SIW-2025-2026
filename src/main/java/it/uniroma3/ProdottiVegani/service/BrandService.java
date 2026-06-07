090a22cc567e6222a7370b9176fa480e6508e93f
BeatriceCiucciarelli <bea.ciucciarelli@stud.uniroma3.it>
2026-06-07 22:24:58 +0200

    Update BrandService.java

--- src/main/java/it/uniroma3/ProdottiVegani/service/BrandService.java
+++ src/main/java/it/uniroma3/ProdottiVegani/service/BrandService.java
@@ -1,8 +1,41 @@
 package it.uniroma3.ProdottiVegani.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.ProdottiVegani.model.Brand;
+import it.uniroma3.ProdottiVegani.repository.BrandRepository;
 
 @Service
 public class BrandService {
+	private BrandRepository brandRepository;
 
+    public BrandService(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
+    
+    @Transactional(readOnly = true)
+    public List<Brand> findAll() {
+        List<Brand> brands = new ArrayList<>();
+        this.brandRepository.findAll().forEach(brands::add);
+        return brands;
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Brand> findById(Long id) {
+        return this.brandRepository.findById(id);
+    }
+    
+
+    public Brand save(Brand brand) {
+        return this.brandRepository.save(brand);
+    }
+
+    public void deleteById(Long id) {
+        this.brandRepository.deleteById(id);
+    }
 }
