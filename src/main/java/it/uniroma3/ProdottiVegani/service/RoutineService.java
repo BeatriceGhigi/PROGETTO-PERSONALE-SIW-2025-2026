b40dac67e22d00e4c7d529ac57c530be6403d910
BeatriceCiucciarelli <bea.ciucciarelli@stud.uniroma3.it>
2026-06-07 22:45:31 +0200

    Update RoutineService.java

--- src/main/java/it/uniroma3/ProdottiVegani/service/RoutineService.java
+++ src/main/java/it/uniroma3/ProdottiVegani/service/RoutineService.java
@@ -1,8 +1,32 @@
 package it.uniroma3.ProdottiVegani.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.ProdottiVegani.model.Routine;
+import it.uniroma3.ProdottiVegani.repository.RoutineRepository;
 
 @Service
 public class RoutineService {
+	private RoutineRepository routineRepository;
+	
+	public RoutineService(RoutineRepository routineRepository) {
+		this.routineRepository=routineRepository;
+	}
+	
+	@Transactional(readOnly = true)
+    public List<Routine> findAll() {
+        List<Routine> routine = new ArrayList<>();
+        this.routineRepository.findAll().forEach(routine::add);
+        return routine;
+    }
 
+    @Transactional(readOnly = true)
+    public Optional<Routine> findById(Long id) {
+        return this.routineRepository.findById(id);
+    }
 }
