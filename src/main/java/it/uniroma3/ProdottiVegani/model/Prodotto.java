91a558eaa5f4d35897b6406becb49d537f89bf73
BeatriceGhigi <beaghigi44@gmail.com>
2026-06-08 17:38:55 +0200

    commit

--- src/main/java/it/uniroma3/ProdottiVegani/model/Prodotto.java
+++ src/main/java/it/uniroma3/ProdottiVegani/model/Prodotto.java
@@ -2,7 +2,7 @@ package it.uniroma3.ProdottiVegani.model;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
+
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,7 +11,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
+
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
