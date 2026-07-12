15b4590475545b0ebff0b37f14ccb66e08f51331
BeatriceCiucciarelli <bea.ciucciarelli@stud.uniroma3.it>
2026-07-12 19:12:11 +0200

    Update import.sql
    correzione errore import e query

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,42 +15,40 @@ INSERT INTO utente (username, password, ruolo, skin_type) VALUES ('admin', '$2b$
 INSERT INTO utente (username, password, ruolo, skin_type) VALUES ('mario', '$2b$10$0pwGs.WGOqZBTunkLLVfKuW05Y5xKWoIRGYv/HgezDSa22BXC4nZW', 'USER', 'NORMALE');
 
 
--- Altri 2 brand (id 4, 5)
 INSERT INTO brand (nome, descrizione, paese, sito_web) VALUES ('PureLeaf', 'Brand britannico di cosmesi naturale a basso impatto ambientale.', 'Regno Unito', 'https://www.pureleaf.co.uk');
 INSERT INTO brand (nome, descrizione, paese, sito_web) VALUES ('Veganza', 'Linea spagnola di skincare 100% vegana e cruelty free.', 'Spagna', 'https://www.veganza.es');
 
--- Altre 2 categorie (id 4, 5)
+
 INSERT INTO categoria (nome, descrizione) VALUES ('Shampoo Solido', 'Shampoo in formato solido, senza plastica e a basso impatto.');
 INSERT INTO categoria (nome, descrizione) VALUES ('Maschera Viso', 'Trattamenti intensivi settimanali per il viso.');
 
--- Altri 3 ingredienti (id 4, 5, 6)
+
 INSERT INTO ingrediente (nome, descrizione, funzione, naturale) VALUES ('Olio di Cocco', 'Olio vegetale nutriente, ricco di acidi grassi saturi.', 'EMOLLIENTE', true);
 INSERT INTO ingrediente (nome, descrizione, funzione, naturale) VALUES ('Acido Ialuronico', 'Molecola idratante in grado di trattenere grandi quantità di acqua.', 'IDRATANTE', false);
 INSERT INTO ingrediente (nome, descrizione, funzione, naturale) VALUES ('Tè Verde', 'Estratto vegetale ricco di polifenoli dall''azione antiossidante.', 'ANTIOSSIDANTE', true);
 
--- Altri 5 prodotti (id 4..8)
+
 INSERT INTO prodotto(nome, descrizione, prezzo, formato, vegan, cruelty_free, skin_type, brand_id, categoria_id) VALUES('Crema Notte Rigenerante', 'Crema viso ricca per la notte, formulata per pelli miste.', 15.90, '50ml', true, true, 'MISTA', 3, 1);
 INSERT INTO prodotto(nome, descrizione, prezzo, formato, vegan, cruelty_free, skin_type, brand_id, categoria_id) VALUES('Maschera Argilla Verde', 'Maschera purificante all''argilla verde per pelli grasse.', 8.50, '100ml', true, true, 'GRASSA', 2, 5);
 INSERT INTO prodotto(nome, descrizione, prezzo, formato, vegan, cruelty_free, skin_type, brand_id, categoria_id) VALUES('Shampoo Solido Cocco', 'Shampoo solido nutriente all''olio di cocco, zero plastica.', 11.00, '80g', true, true, 'NORMALE', 4, 4);
 INSERT INTO prodotto(nome, descrizione, prezzo, formato, vegan, cruelty_free, skin_type, brand_id, categoria_id) VALUES('Siero Acido Ialuronico', 'Siero viso ad alta concentrazione di acido ialuronico.', 19.90, '30ml', true, true, 'SECCA', 5, 1);
 INSERT INTO prodotto(nome, descrizione, prezzo, formato, vegan, cruelty_free, skin_type, brand_id, categoria_id) VALUES('Detergente Tè Verde', 'Detergente viso quotidiano con estratto di tè verde.', 10.50, '150ml', true, true, 'MISTA', 1, 2);
 
--- Altri 2 utenti (id 3, 4)
+
 INSERT INTO utente (username, password, ruolo, skin_type) VALUES ('giulia', '$2b$10$0pwGs.WGOqZBTunkLLVfKuW05Y5xKWoIRGYv/HgezDSa22BXC4nZW', 'USER', 'SECCA');
 INSERT INTO utente (username, password, ruolo, skin_type) VALUES ('luca', '$2b$10$0pwGs.WGOqZBTunkLLVfKuW05Y5xKWoIRGYv/HgezDSa22BXC4nZW', 'USER', 'GRASSA');
 
--- Collegamenti prodotto <-> ingrediente (tabella di join del @ManyToMany).
 
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (1, 1); -- Crema Viso Aloe Bio - Aloe Vera
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (2, 2); -- Detergente Camomilla - Camomilla
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (3, 3); -- Burrocacao Argan - Olio di Argan
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (4, 3); -- Crema Notte - Olio di Argan
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (4, 5); -- Crema Notte - Acido Ialuronico
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (5, 2); -- Maschera Argilla - Camomilla
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (6, 4); -- Shampoo Solido - Olio di Cocco
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (7, 5); -- Siero Ialuronico - Acido Ialuronico
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (8, 6); -- Detergente Tè Verde - Tè Verde
-INSERT INTO prodotto_ingrediente (prodotto_id, ingrediente_id) VALUES (8, 1); -- Detergente Tè Verde - Aloe Vera
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (1, 1); -- Crema Viso Aloe Bio - Aloe Vera
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (2, 2); -- Detergente Camomilla - Camomilla
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (3, 3); -- Burrocacao Argan - Olio di Argan
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (4, 3); -- Crema Notte - Olio di Argan
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (4, 5); -- Crema Notte - Acido Ialuronico
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (5, 2); -- Maschera Argilla - Camomilla
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (6, 4); -- Shampoo Solido - Olio di Cocco
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (7, 5); -- Siero Ialuronico - Acido Ialuronico
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (8, 6); -- Detergente Tè Verde - Tè Verde
+INSERT INTO prodotto_ingredienti (prodotti_id, ingredienti_id) VALUES (8, 1); -- Detergente Tè Verde - Aloe Vera
 
 
 INSERT INTO recensione (voto, commento, data_creazione, data_ultima_modifica, prodotto_id, utente_id) VALUES (5, 'Ottima crema, assorbita subito e non unge.', '2026-05-10 09:30:00', '2026-05-10 09:30:00', 1, 2);
